a8118e56b879246e9aa553cd13c7590defa22cc4
ishtvankomka <ishtvan.komonii.it.2021@lpnu.ua>
2023-10-11 16:16:54 +0300

    Refactor: lab3

--- lab3/main.py
+++ lab3/main.py
@@ -1,29 +1,7 @@
 from custom_text_art_interface.custon_text_art_interface import CustomTextArtInterface
-import pyfiglet
-from termcolor import colored
-import re
 
 
 def main():
-    """ text = input("your text: ")
-    font_name = input("art font: ")
-    symbol_to_replace = input("art symbol(skip for default font symbols): ")
-    color = input("art color: ")
-    art_width = int(input("art width: "))
-
-    figlet = pyfiglet.Figlet(font=font_name, width=art_width)
-
-    ascii_art_text = figlet.renderText(text)
-
-    pattern = r'\S'
-    replaced_ascii_art_text = re.sub(pattern, symbol_to_replace, ascii_art_text)
-    colored_art_text =  colored(replaced_ascii_art_text, color)
-
-    print("your art:\n")
-    print(ascii_art_text)
-    print(replaced_ascii_art_text)
-    print(colored_art_text) """
-    
     custon_text_art_interface = CustomTextArtInterface()
     custon_text_art_interface.launch()
 
