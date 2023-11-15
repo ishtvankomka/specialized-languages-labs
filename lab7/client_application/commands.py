51d787b24f1f93486568a8f11e7fd6d77e8a25ad
ishtvankomka <ishtvan.komonii.it.2021@lpnu.ua>
2023-11-15 03:31:01 +0200

    Chore: moved classes to packages

--- lab7/client_application/commands.py
+++ lab7/client_application/commands.py
@@ -1,18 +1,51 @@
 from command.i_command import ICommand
 
 
-class Generate3DFigureCommand(ICommand):
-    def __init__(self, figure_interface):
-        self.figure_interface = figure_interface
+class GetApiDataCommand(ICommand):
+    def __init__(self, _app_receiver, route, size, history, display_type, color):
+        self._app_receiver = _app_receiver
+        self.route = route
+        self.size = size
+        self.history = history
+        self.display_type = display_type
+        self.color = color
 
     def execute(self):
-        print(self.figure_interface.generate_3d_figure())
+        self._app_receiver.run_get_api_data(
+            self.route, self.size, self.history, self.display_type, self.color)
 
 
-class SetSizeCommand(ICommand):
-    def __init__(self, figure_interface, new_size):
-        self.figure_interface = figure_interface
-        self.new_size = new_size
+class GetHistoryDataCommand(ICommand):
+    def __init__(self, _app_receiver, history, display_type, color):
+        self._app_receiver = _app_receiver
+        self.history = history
+        self.display_type = display_type
+        self.color = color
 
     def execute(self):
-        self.figure_interface.set_size(self.new_size)
+        self._app_receiver.run_get_history_data(
+            self.history, self.display_type, self.color)
+
+
+class SaveDataToTxtFileCommand(ICommand):
+    def __init__(self, _app_receiver, history, display_type, filename):
+        self._app_receiver = _app_receiver
+        self.history = history
+        self.display_type = display_type
+        self.filename = filename
+
+    def execute(self):
+        self._app_receiver.run_save_data_to_txt_file(
+            self.history, self.display_type, self.filename)
+
+
+class SaveDataToJsonFileCommand(ICommand):
+    def __init__(self, _app_receiver, history, display_type, filename):
+        self._app_receiver = _app_receiver
+        self.history = history
+        self.display_type = display_type
+        self.filename = filename
+
+    def execute(self):
+        self._app_receiver.run_save_data_to_json_file(
+            self.history, self.display_type, self.filename)
